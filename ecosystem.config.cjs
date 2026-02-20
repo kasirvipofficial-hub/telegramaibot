1765c906b6a525aa20fc8df2dcc2e75cf111f52a
asifdzakiy-droid <asifdzakiy@gmail.com>
2026-02-20 13:55:56 +0700

    Fix PM2 cwd for monorepo support

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,27 +2,27 @@ module.exports = {
     apps: [
         {
             name: 'bot-engine',
-            script: './bot-engine/server.js',
-            cwd: './',
+            script: './server.js',
+            cwd: './bot-engine',
             watch: false,
             env: {
                 NODE_ENV: 'production'
             },
             log_date_format: 'YYYY-MM-DD HH:mm:ss',
-            error_file: './logs/bot-error.log',
-            out_file: './logs/bot-out.log',
+            error_file: '../logs/bot-error.log',
+            out_file: '../logs/bot-out.log',
         },
         {
             name: 'video-engine',
-            script: './video-engine/src/server.js',
-            cwd: './',
+            script: './src/server.js',
+            cwd: './video-engine',
             watch: false,
             env: {
                 NODE_ENV: 'production'
             },
             log_date_format: 'YYYY-MM-DD HH:mm:ss',
-            error_file: './logs/video-error.log',
-            out_file: './logs/video-out.log',
+            error_file: '../logs/video-error.log',
+            out_file: '../logs/video-out.log',
         }
     ]
 };
